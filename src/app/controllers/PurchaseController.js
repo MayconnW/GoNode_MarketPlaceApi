3feac06826a4268fa8fdcbdc44a3578cfd6197b1
Mayconn Oliveira <mayconnwgo@gmail.com>
2019-05-07 18:14:21 -0300

    Start to develop Purchase feature

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,5 +1,6 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
+const Purchase = require('../models/Purchase')
 const PurchaseMail = require('../jobs/PurchaseMail')
 const Queue = require('../services/Queue')
 
@@ -9,6 +10,8 @@ class PurchaseController {
 
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
+
+    await Purchase.create({ ...req.body, buyer: req.userId })
 
     Queue.create(PurchaseMail.key, {
       ad: purchaseAd,
